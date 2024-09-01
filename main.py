4da2614699898e13b298e9e3e4ba9bf65994aaaa
arsh-gupta-8 <SlurpIt.MBMS@gmail.com>
2024-09-01 23:05:32 +0700

    Added background and shooting in btoh directions

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ HEIGHT = 550
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tower Defense")
 
+# Images
+Grassland_BG = pygame.image.load('Background/Grassland.png')
+
 # Loop Start
 clock = pygame.time.Clock()
 running = True
@@ -21,17 +24,34 @@ gravity = -0.75
 # Player Settings
 playerx = 500
 playery = 225
+facingRight = True
 player_ground = True
 down_vel = 0
+bullets = []
 
-def player_movement(playerx, playery, player_ground, down_vel):
+class Bullet:
+    def __init__(self, bulletx, bullety, facingRight):
+        self.bulletx = bulletx
+        self.bullety = bullety
+        self.facingRight = facingRight
+        self.speed = 20
+
+    def update_bullet_position(self):
+        if self.facingRight:
+            self.bulletx += 20
+        else:
+            self.bulletx -= 20
+
+def player_movement(playerx, playery, player_ground, down_vel, facingRight):
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] or keys[pygame.K_a] and w:
+    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and playerx > 0:
         playerx -= 10
+        facingRight = False
 
-    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+    if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and playerx < WIDTH - 58:
         playerx += 10
+        facingRight = True
 
     if (keys[pygame.K_UP] or keys[pygame.K_w]) and player_ground:
         down_vel = 17
@@ -44,16 +64,18 @@ def player_movement(playerx, playery, player_ground, down_vel):
     else:
         down_vel = 0
 
-    if playery < 400:
+    if playery < 385:
         player_ground = False
     else:
         player_ground = True
 
-    return playerx, playery, player_ground, down_vel
+    return playerx, playery, player_ground, down_vel, facingRight
 
 while running:
 
-    playerx, playery, player_ground, down_vel = player_movement(playerx, playery, player_ground, down_vel)
+    screen.blit(Grassland_BG, (0, 0))
+
+    playerx, playery, player_ground, down_vel, facingRight = player_movement(playerx, playery, player_ground, down_vel, facingRight)
 
     x, y = pygame.mouse.get_pos()
 
@@ -65,11 +87,19 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+            elif event.key == pygame.K_SPACE:
+                bullets.append(Bullet(playerx, playery, facingRight))
 
         elif event.type == pygame.MOUSEBUTTONUP:
             pass
 
-    screen.fill((0, 0, 0))
+    for bullet in bullets:
+        bullet.update_bullet_position()
+        pygame.draw.rect(screen, (0, 0, 0), [bullet.bulletx, bullet.bullety, 25, 10])
+        if bullet.bulletx < -25 or bullet.bulletx > WIDTH:
+            bullets.remove(bullet)
+
+    # screen.fill((0, 0, 0))
     pygame.draw.rect(screen, (255, 255, 255), [playerx, playery, 48, 64])
     pygame.display.update()
     clock.tick(FPS)
